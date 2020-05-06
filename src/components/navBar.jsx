ace7e43d328858f47f464d83eecedf9448af707c
sastaachar <justinjohnmathew@karunya.edu.in>
2020-05-06 19:40:41 +0530

    Demo

--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -5,7 +5,7 @@ const NavBar = (props) => {
   return (
     <nav className="navbar sticky-top navbar-dark bg-dark ">
       <span className="text-center  navbar-brand mb-0 h1 ">
-        UI is under construction
+        UI is under construction BITCH
       </span>
     </nav>
   );
